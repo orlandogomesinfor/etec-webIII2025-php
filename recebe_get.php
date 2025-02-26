05c05faa3a46df42ae9da1ff1b14cd8c63c8f421
orlandogomesinfor <104329660+orlandogomesinfor@users.noreply.github.com>
2025-02-26 17:09:59 -0300

    Segunda lista de exercícios e exemplos de form
    Temos aqui a segunda lista de exercícios e exemplos de captura de dados através do form GET/POST

---
+++ recebe_get.php
@@ -0,0 +1,9 @@
+<?php
+echo "==============================<br>";
+echo "======= Recebimento de dados =======<br>";
+echo "==============================<br>";
+$nome  = $_GET["nome"];
+$email = $_GET["email"];
+echo "Voce digitou o nome: $nome";
+echo "<br>email: $email"; 
+?>
---
+++ recebe_post.php
@@ -0,0 +1,9 @@
+<?php
+//Teste formulário Post
+$nomE   = $_POST['nome'];
+$bairro = $_POST['bairro'];
+echo " *** Dados Recebido ***<br>";
+echo "Nome",": ",$nomE,"<br>";
+echo "bairro $bairro<br>";
+echo " ===================== ";
+?>
